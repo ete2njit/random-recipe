13614b373933eae3b1c637c5dc1285b9f5d465e1
EC2 Default User <ec2-user@ip-172-31-59-156.ec2.internal>
2020-09-28 22:09:13 +0000

    Add spoonacular functionality and update readme and html accordingly.

--- README.md
+++ README.md
@@ -1,24 +1,31 @@
 To use this repository, you must follow these steps:
 
-0. Sign up for the twitter developer portal at https://developer.twitter.com
-1. Navigate to https://developer.twitter.com/en/portal/projects-and-apps and make a new app.
-2. Click on the key symbol after creating your project, and it will take you to your keys and tokens.
-    If needed, you can regenerate your access token and secret.
-3. Clone this repository by using git clone thttps://github.com/NJIT-CS490/project1-ete2.git
-4. Run the following in your terminal:
-    sudo pip install tweepy
-    (or) sudo pip3 install tweepy
-    (or) pip install tweepy
-    (or) pip3 install tweepy
-5. Install flask using the same process as above ([sudo] pip[3] install flask)
-7. Add your secret keys (from step 2) by making a new root-level file called twitter.env and populating it as follows.
-    **** MAKE SURE THE FILE AND VARIABLES ARE NAMED THE EXACT SAME WAY AS DESCRIBED!!!***
+0.  Sign up for the twitter developer portal at https://developer.twitter.com
+1.  Navigate to https://developer.twitter.com/en/portal/projects-and-apps and make a new app.
+2.  Click on the key symbol after creating your project, and it will take you to your keys and tokens.
+     If needed, you can regenerate your access token and secret.
+3.  Sign up at https://spoonacular.com/food-api
+4.  Locate your Spoonacular API key under Profile in the API console.
+5.  Clone this repository by using git clone thttps://github.com/NJIT-CS490/project1-ete2.git
+6.  Run the following in your terminal:
+     sudo pip install tweepy
+     (or) sudo pip3 install tweepy
+     (or) pip install tweepy
+     (or) pip3 install tweepy
+7.  Install flask using the same process as above ([sudo] pip[3] install flask)
+8.  Add your secret keys (from step 2) by making a new root-level file called twitter.env and populating it as follows.
+     **** MAKE SURE THE FILE AND VARIABLES ARE NAMED THE EXACT SAME WAY AS DESCRIBED!!!***
+     export KEY=''
+     export KEY_SECRET=''
+     export TOKEN=''
+     export TOKEN_SECRET=''
+9.  In the terminal, run 'source twitter.env'
+10. Add your secret keys (from step 2) by making a new root-level file called spoon.env and populating it as follows.
+    **** MAKE SURE THE FILE AND VARIABLE ARE NAMED THE EXACT SAME WAY AS DESCRIBED!!!***
     export KEY=''
-    export KEY_SECRET=''
-    export TOKEN=''
-    export TOKEN_SECRET=''
-8. Run `python main.py`
-9. If on Cloud9, preview templates/index.html. This should successfully render the HTML!
+11. In the terminal, run 'source spoon.env'
+12. Run `python main.py`
+13. If on Cloud9, preview templates/index.html. This should successfully render the HTML!
 
 
 
@@ -31,6 +38,10 @@ changes to the CSS file were being picked up again (most of the time, as elabora
 to './' originally as the files original name was simply 'style.css', which seems to have gotten mixed up sometimes with a likewise named
 file in a different folder. I later decided to simply rename my current CSS file to 'tstyle.css', to avoid this confusion altogether, 
 although I find it to be an odd problem and unscalable solution to make every CSS filename unique despite being in different folders.
+
+
+I had some issue getting my spoonacular to work, the main problem being figuring out what the response was/why is was unsubscriptable.
+Thanks to stackoverflow, I reasonably quickly found converting it to json was the way to go.
 
 Known Issues:
 
--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 from tweepy import OAuthHandler
 from tweepy import API
 from tweepy import Cursor
-#from collections import Counter
 import requests
 import flask
 import sys
 import os
 import random
-
 
 
 app = flask.Flask(__name__)
@@ -16,17 +14,72 @@ consumer_key=os.environ['TWITTER_API']
 consumer_secret=os.environ['TWITTER_API_SECRET']
 access_token=os.environ['TWITTER_ACCESS']
 access_token_secret=os.environ['TWITTER_ACCESS_SECRET']
+spoon_key = os.environ['SPOONACULAR_KEY']
 
 auth = OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 auth_api = API(auth)
 
+# stores the selection of foods we randomly choose from to find a recipe and one (or more) tweet(s)
+foods = ['Pork Chop', 'General Tso\'s Chicken', 'Pizza', 'Sushi', 'Beef Stew', 'Chicken Noodle Soup', 'Pasta e fagioli', 'Brownies', 'Dumplings']
 
-foods = ['Pork Chop', 'General Tso\'s Chicken', 'Pineapple Pizza', 'Sushi', 'Beef Stew', 'Chicken Noodle Soup', 'Pasta e fagioli', 'Brownies', 'Dumplings', 'Chicken Sandwhich']
+
 # I am using an array to store tweets in case I want to eventually display more than one, 
 # or simply to have a few tweets to choose at random from 
 max_tweets = 1
 
+def get_recipe(query):
+    # First, find a random recipe based on the query prompt
+    
+    # https://spoonacular.com/food-api/docs#Authentication
+    # https://spoonacular.com/food-api/docs#Search-Recipes-Complex
+    link = "https://api.spoonacular.com/recipes/complexSearch?apiKey=" + spoon_key + "&query=" + query + "&number=1"
+    response = requests.get(link)
+    # https://stackoverflow.com/questions/34508981/response-object-is-not-subscriptable-python-http-post-request/34509116
+    data = response.json()
+    
+    dish = data["results"][0]
+    
+    # Next, we find the actual recipe for the dish
+    # https://spoonacular.com/food-api/docs#Get-Recipe-Information
+    link = "https://api.spoonacular.com/recipes/" + "656729" + "/information?apiKey=" + spoon_key + "&includeNutrition=false"
+    response = requests.get(link)
+    recipe_json = response.json()
+    recipe = recipe_json["extendedIngredients"]
+    
+    recipe_list = []
+    
+    for i in range(0, len(recipe)):
+        recipe_list.append(recipe[i]["original"])
+    
+    print(recipe_list)
+    print()
+    print(recipe_json["summary"])
+    
+    
+    dish_info = []
+    dish_info.append(dish["image"])
+    dish_info.append(dish["title"])
+    dish_info.append(recipe_json["summary"])
+    dish_info.append(recipe_list)
+    
+    return dish_info
+    
+# --------------------------------------------------------------------------------------------
+def get_recipe_placebo(query):
+    
+    recipe_list = ['3 apples', '3 tsps Dijon mustard', '2 garlic cloves chopped finely', '4 tsps honey', 'Juice of a lemon', '1 tbsp olive oil', '4 pork chops', 'Salt and pepper', '1 large white onion sliced into thin rings']
+    
+    recipe_summary = 'Pork Chop with Honey, Mustard and Apples might be just the main course you are searching for. This caveman, gluten free, dairy free, and primal recipe serves 4 and costs <b>$2.4 per serving</b>. One serving contains <b>353 calories</b>, <b>30g of protein</b>, and <b>13g of fat</b>. If you have honey, onion, salt and pepper, and a few other ingredients on hand, you can make it. From preparation to the plate, this recipe takes about <b>45 minutes</b>. Only a few people made this recipe, and 1 would say it hit the spot. All things considered, we decided this recipe <b>deserves a spoonacular score of 59%</b>. This score is solid. Try <a href="https://spoonacular.com/recipes/mashed-sweet-potatoes-pork-chop-with-cider-gravy-sauteed-apples-and-onions-749564">Mashed Sweet Potatoes, Pork Chop with Cider Gravy, Sauteed Apples and Onions</a>, <a href="https://spoonacular.com/recipes/honey-mustard-chicken-and-apples-296098">Honey-Mustard Chicken and Apples</a>, and <a href="https://spoonacular.com/recipes/honey-mustard-coleslaw-with-apples-167187">Honey-Mustard Coleslaw with Apples</a> for similar recipes.'
+    
+    dish_info = []
+    dish_info.append("https://spoonacular.com/recipeImages/656729-312x231.jpg")
+    dish_info.append("Pork Chop with Honey, Mustard and Apples")
+    dish_info.append(recipe_summary)
+    dish_info.append(recipe_list)
+    
+    return dish_info
+    
 
 @app.route('/') # Python decorator
 def homepage():
@@ -36,6 +89,9 @@ def homepage():
     while(not results):
         # pick a random recipe from our list
         query = random.choice(foods)
+        
+        dish_info = get_recipe_placebo(query)
+        
         # find a number of tweets related to the recipe we chose
         results = auth_api.search(q = query, count = max_tweets, lang = "en")
     
@@ -54,7 +110,9 @@ def homepage():
             "index.html",
             food = query,
             tweet = tweets[0],
-            tweets_len = len(tweets)
+            tweets_len = len(tweets),
+            dish_info = dish_info,
+            dish_info_length = len(dish_info)
         ) 
 
 app.run(
